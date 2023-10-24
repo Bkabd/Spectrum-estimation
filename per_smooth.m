b21b399c3f32d45ac7d487961befb243ae78840e
Bkabd <142501358+Bkabd@users.noreply.github.com>
2023-10-24 12:02:02 +0530

    Update per_smooth.m

--- per_smooth.m
+++ per_smooth.m
@@ -2,14 +2,13 @@ function [Px,w] = per_smooth(x,wind,M)
 % Blackman-Tukey Method
 % x: Input signal
 % wind: The window sequence
-% M: Thwindow's half length
-
-x=x(:);
-r=1/M*xcorr(x(1:M),x(1:M));
-M=2*M-1;
-r=r.*wind;
-[Pxx,w]=freqz(r,1);
-Px=abs(Pxx);
+% M: Thwindow's half length    
+    x=x(:);
+    r=1/M*xcorr(x(1:M),x(1:M));
+    M=2*M-1;
+    r=r.*wind;
+   [Pxx,w]=freqz(r,1);
+    Px=abs(Pxx);
 
 
 
